a6bfbdf64bb572ac142c4fc32eb4c60cd3c67114
Jeff <jeffreysaa@outlook.com>
2017-09-05 07:06:42 -0500

    QuickEditMode ready in variants

--- js/controllers/ShowProductController.js
+++ js/controllers/ShowProductController.js
@@ -9,10 +9,14 @@ angular.module('KanguDashboard', ['ngFileUpload']).controller('ShowProductContro
 	$rootScope.settings.layout.pageBodySolid = false;
 	$rootScope.settings.layout.pageSidebarClosed = false;
 
+	$scope.editing_mode = false;
+
 	$scope.get_product = function(){
 		$http({method: 'GET', url: $rootScope.server()+'products/products/'+$stateParams.id, headers:{"Authorization":$rootScope.loadUser().token}
 		}).then(function successCallback(response) {
 			$scope.product = response.data;
+			if ($scope.editing_mode)
+				$scope.temp_fields();
 		}, function errorCallback(response) {
 			toastr.error('Error obteniendo datos');
 		});
@@ -43,7 +47,14 @@ angular.module('KanguDashboard', ['ngFileUpload']).controller('ShowProductContro
 		angular.copy(v, $scope.temp_variant);
 	}
 
-	$scope.updateVariant = function($event, variant){
+	$scope.updateVariant = function($event, variant, sw){
+		if (sw) {
+			variant.entry_price = variant.t_entry_price;
+			variant.natural_percent = variant.t_natural_percent;
+			variant.natural_gain = variant.t_natural_gain;
+			variant.business_percent = variant.t_business_percent;
+			variant.business_gain = variant.t_business_gain;
+		}
 		variant.product_id = $stateParams.id;
 		Upload.upload({
 			url:  $rootScope.server()+'variants/variants/'+variant.id,
@@ -58,6 +69,23 @@ angular.module('KanguDashboard', ['ngFileUpload']).controller('ShowProductContro
 		}, function (evt) {
 			var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
 		});
+		console.log(variant);
+	}
+
+	$scope.editMode = function(){
+		$scope.editing_mode = !$scope.editing_mode;
+		$scope.temp_fields();
+	}
+
+	$scope.temp_fields = function(){
+		for (var i = $scope.product.variants.length - 1; i >= 0; i--) {
+			var p = $scope.product.variants[i];
+			p.t_entry_price = p.entry_price;
+			p.t_natural_percent = p.natural_percent;
+			p.t_natural_gain = p.natural_gain;
+			p.t_business_percent = p.business_percent;
+			p.t_business_gain = p.business_gain;
+		}
 	}
 
 	$scope.variantImageUrl = $rootScope.variantImageUrl;
